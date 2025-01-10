7c93e07e44d2f6004cdfe55e4bdf7f3eab516445
Tamas Vass <t.vass.ext@sportradar.com>
2025-01-10 11:35:29 +0100

    Add Wpf app variant to InvalidBoundsForSelectedItem

---
+++ InvalidBoundsForSelectedItem/WpfApp_InvalidBoundsForSelectedItem/ViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.ObjectModel;
+
+namespace WpfApp_InvalidBoundsForSelectedItem
+{
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            for (int i = 1; i < 16; i++)
+            {
+                Values.Add(new Data(i));
+            }
+
+            Values.Add(new DataEx(-1, true));
+
+            for (int i = 16; i < 31; i++)
+            {
+                Values.Add(new Data(i));
+            }
+        }
+
+        public ObservableCollection<Data> Values { get; } = new();
+    }
+
+    public class Data
+    {
+        public Data(int val, bool isSpecial = false)
+        {
+            Value = val;
+            IsSpecial = isSpecial;
+        }
+
+        public bool IsSpecial { get; set; }
+
+        public int Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"Value: {Value} IsSpecial: {IsSpecial}";
+        }
+    }
+
+    public class DataEx : Data
+    {
+        public DataEx(int val, bool isSpecial = false) : base(val, isSpecial)
+        {
+        }
+    }
+}
